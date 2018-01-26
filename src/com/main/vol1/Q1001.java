df5a950db96231eaf78091372b94e7fea291b38a
clatt <358058306@qq.com>
2018-01-26 14:50:14 +0800

    ACM Problems Set

---
+++ src/com/main/vol1/Q1001.java
@@ -0,0 +1,22 @@
+/*
+ * 文件名：P1001.java
+ * 版权：Copyright by www.ixuenong.com
+ * 描述：
+ */
+
+package com.main.vol1;
+
+import java.util.Scanner;
+//http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=1001
+public class Q1001
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String read;
+        while(scan.hasNext())
+        {
+            System.out.println(scan.nextInt() + scan.nextInt());
+        }
+    }
+}
---
+++ src/com/main/vol1/Q1002.java
@@ -0,0 +1,22 @@
+/*
+ * 文件名：P1001.java
+ * 版权：Copyright by www.ixuenong.com
+ * 描述：
+ */
+
+package com.main.vol1;
+
+import java.util.Scanner;
+//http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=1002
+public class Q1002
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String read;
+        while(scan.hasNext())
+        {
+            System.out.println(scan.nextInt() + scan.nextInt());
+        }
+    }
+}
