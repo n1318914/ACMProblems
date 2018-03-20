32f300c48536f8adbc2ab5afdc02ca925cc19854
clatt <358058306@qq.com>
2018-03-20 21:09:18 +0800

    Q0002

--- src/com/main/leetcode/Q0002.java
+++ src/com/main/leetcode/Q0002.java
@@ -6,78 +6,38 @@
 
 package com.main.leetcode;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-
+/**
+ * 
+ * https://leetcode.com/problems/add-two-numbers/description/
+ * 
+ * @author yulong.chen@dfs168.com
+ * @since 2018年3月20日
+ */
 public class Q0002
 {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        BigDecimal l = reverse(l1).add(reverse(l2));
-        if(l.compareTo(new BigDecimal(0)) == 0) 
-        {
-            return new ListNode(0);
-        }
-        ListNode node = sort(null, l);
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        ListNode p = l1,q = l2,curr = head;
         
-        return node;
+        while(null != p || null!= q)
+        {
+            int x = null != q ? q.val : 0;
+            int y = null != p ? p.val : 0;
+            int z = x + y + carry;
+            curr.next = new ListNode(z % 10);
+            carry = z / 10;
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if(carry > 0)
+        {
+            curr.next = new ListNode(carry);
+        }
+        return head.next;
     }
     
-    public static ListNode sort(ListNode node,BigDecimal l)
-    {
-        if(l.compareTo(new BigDecimal(0)) == 0)
-        {
-            return node;
-        }
-        int lenth = l.toString().length();
-        if(lenth > 0)
-        {
-            BigDecimal[] divideAndRemainder = l.divideAndRemainder(new BigDecimal(10));
-            if(null == node)
-            {
-                node = new ListNode(divideAndRemainder[1].intValue());
-                sort(node, divideAndRemainder[0]);
-            }
-            else
-            {
-                node.next = new ListNode(divideAndRemainder[1].intValue());
-                if(divideAndRemainder[0].compareTo(new BigDecimal(0)) != 0)
-                {
-                    sort(node.next, divideAndRemainder[0]);
-                }
-            }
-        }
-        return node;
-    }
-    public static BigDecimal reverse(ListNode l)
-    {
-        String str="";
-        while(null != l.next)
-        {
-            str = l.val + str;
-            l = l.next;
-        }
-        str = l.val + str;
-        return new BigDecimal(str);
-    }
-    public static void main(String[] args)
-    {
-        ListNode l11 = new ListNode(1);
-        ListNode l12 = new ListNode(8);
-//        ListNode l13 = new ListNode(6);
-        l11.next = l12;
-//        l12.next = l13;
-        
-        
-        
-        ListNode l21 = new ListNode(0);
-//        ListNode l22 = new ListNode(6);
-//        ListNode l23 = new ListNode(4);
-//        l21.next = l22;
-//        l22.next = l23;
-        
-        ListNode l = addTwoNumbers(l11,l21);
-        
-    }
 }
 
 class ListNode {
